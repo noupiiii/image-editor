d61fd1a8f3f9a6823e22d0f53deaca44399ba562
noupiiii <louisbruche2022@gmail.com>
2024-12-18 22:53:12 +0100

    docker

--- API/app/core/config.py
+++ API/app/core/config.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     origins = [
         "http://frontend:3000",
-        "http://54.36.101.216/",
+        "http://54.36.101.216",
     ]
     app.add_middleware(
         CORSMiddleware,
