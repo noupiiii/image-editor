40270366752fc8bbd14d49dd7b8a2ee9af28e079
noupiiii <louisbruche2022@gmail.com>
2024-12-18 23:01:29 +0100

    cors origin change

--- API/app/core/config.py
+++ API/app/core/config.py
@@ -4,7 +4,8 @@ def setup_cors(app):
     origins = [
         "http://frontend:3000",
         "http://54.36.101.216",
-        "*"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ]
     app.add_middleware(
         CORSMiddleware,
